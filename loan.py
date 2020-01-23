bdfda4e57c94ffef8be57c1fe11e3bc55d4fdf1d
Prosper <58360630+Kessie2862@users.noreply.github.com>
2020-01-23 07:27:30 +0000

    Add files via upload

--- loan.py
+++ loan.py
@@ -1,1 +1,12 @@
-print("Hello")
+income = float(input("How much do you earn each month? "))
+principal = float(input("How much loan do you need? "))
+time = int(input("How many month(s) will it take to repay the loan? "))
+rate = 3
+
+annual_interest = (principal * rate * time) / 100
+monthly_interest = annual_interest / 12
+
+if income < principal / 20:
+    print("Sorry, you don't qualify for the loan")
+elif income >= principal / 20:
+     print(f"You qualify for the loan of {principal} cedis at {monthly_interest} cedis interest at the end of {time} month(s)")
